cfd615f6efaf0840a9b01b496e1df121509b3a15
Akshat Mehta <akshatmehta220@gmail.com>
2025-07-10 15:56:54 +0530

    inheriting thymeleaf template

--- thymeleaf/thymeleafproject/src/main/java/com/practice/controller/MyController.java
+++ thymeleaf/thymeleafproject/src/main/java/com/practice/controller/MyController.java
@@ -12,31 +12,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
-    
+
     @RequestMapping(value = "/about", method = RequestMethod.GET)
     public String about(Model model) {
         System.out.println("Inside about handler...");
-        
+
         //putting data in model
         model.addAttribute("name","Akshat");
         model.addAttribute("currentDate",LocalDate.now());
-        
+
         return "about";
     }
-    
+
     //handling iteration
     @GetMapping("/example-loop")
     public String iterateHandler(Model m) {
-    	
+
     	//create a list
     	List<String> names = List.of("Akshat","laxman","Mohit","Varun");
     	m.addAttribute("names", names);
-    	
+
     	return "iterate";
     }
-    
+
    //handler for conditional statement
-    
+
     @GetMapping("/condition")
     public String conditionHandler(Model model) {
     	System.out.println("conditinal handler executed...");
@@ -44,15 +44,21 @@ public class MyController {
     	model.addAttribute("gender","M");
     	return "condition";
     }
-    
+
     //handler for including fragment
-    
+
     @GetMapping("/service")
     public String serviceHandler(Model m) {
-    	
+
     	m.addAttribute("title","I like king");
     	m.addAttribute("subtitle",LocalDate.now().toString());
     	return "service";
     }
-    
+
+    //for new about
+    @GetMapping("/newAbout")
+    public String newAbout() {
+    	return "aboutnew";
+    }
+
 }
