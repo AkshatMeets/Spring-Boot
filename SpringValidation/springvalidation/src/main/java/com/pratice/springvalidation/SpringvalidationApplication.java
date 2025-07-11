cbc69bc08a72ce3d7b856847e50683761f332528
Akshat Mehta <akshatmehta220@gmail.com>
2025-07-11 13:40:24 +0530

    add validation in server side

---
+++ SpringValidation/springvalidation/src/main/java/com/pratice/springvalidation/SpringvalidationApplication.java
@@ -0,0 +1,14 @@
+package com.pratice.springvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class SpringvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvalidationApplication.class, args);
+	}
+
+}
