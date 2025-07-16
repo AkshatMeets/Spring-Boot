34e8e69504cdba92687c6c37d35affd2899b212b
Akshat Mehta <akshatmehta220@gmail.com>
2025-07-16 12:58:21 +0530

    email api

---
+++ SendEmailApi/src/main/java/com/emailapi/App.java
@@ -0,0 +1,86 @@
+package com.emailapi;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.sql.SQLOutput;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("preparing to send message...");
+        String message="Message for security check...";
+        String subject="CodersArea: Confirmation";
+        String to ="akshatmehta220@gmail.com";
+        String from = "akshatmehta222@gmail.com";
+
+//        sendEmail(message,subject,to,from);
+        sendAttach(message,subject,to,from);
+    }
+
+    //responsible to send message with attachment
+    private static void sendAttach(String message, String subject, String to, String from) {
+
+
+    }
+
+    //responsible to send email
+    private static void sendEmail(String message, String subject, String to, String from) {
+        //variable for gmail host
+        String host="smtp.gmail.com";
+
+        //get the system properties
+        Properties properties=System.getProperties();
+        System.out.println("PROPERTIES"+properties);
+
+        //setting imp info to properties
+        properties.put("mail.smtp.host",host);
+        properties.put("mail.smtp.port","465");
+        properties.put("mail.smtp.ssl.enable","true");
+        properties.put("mail.smtp.auth","true");
+
+        //step:1 get the session object
+        Session session=Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("akshatmehta222@gmail.com","lnna kixu cbir fjzm");
+            }
+        });
+
+        session.setDebug(true);
+
+        //step 2: compose the message
+        MimeMessage m=new MimeMessage(session);
+
+        try {
+            //from email
+            m.setFrom(from);
+
+            //adding recipient
+            m.addRecipients(Message.RecipientType.TO, new InternetAddress[]{new InternetAddress(to)});
+
+            //adding subject to message
+            m.setSubject(subject);
+
+            //adding text to message
+            m.setText(message);
+
+            //send
+
+            //step 3: send message using transport class
+
+            Transport.send(m);
+            System.out.println("sent success.....");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
