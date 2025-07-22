92601f249b075a5f02924dbec04d3f0cc20b3a19
Akshat Mehta <akshatmehta220@gmail.com>
2025-07-22 17:43:28 +0530

    Delete workbanao-backend/workbanao-backend directory

--- workbanao-backend/workbanao-backend/src/main/java/com/workbanao/workbanao_backend/entity/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.workbanao.workbanao_backend.entity.enums;
-
-public enum Role {
-    VILLAGER,
-    WORKER,
-    ADMIN
-}
--- workbanao-backend/workbanao-backend/src/main/java/com/workbanao/workbanao_backend/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.workbanao.workbanao_backend.service;
-
-import com.workbanao.workbanao_backend.entity.User;
-import java.util.Optional;
-
-public interface UserService {
-    User registerUser(User user);
-    Optional<User> findByEmail(String email);
-}
